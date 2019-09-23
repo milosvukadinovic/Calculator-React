525a84d8c3828f9754e21b2c1f2b51dfb88f1c09
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-09-23 13:01:43 +0200

    Eslint finually sober

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import './App.css';
-import calculate from './logic/calculate'
+import calculate from './logic/calculate';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
@@ -9,18 +12,20 @@ class App extends React.Component {
     total: null,
     next: null,
     operation: null,
-    error: null
+    error: null,
   }
+
 handleClick = (buttonName) => {
   this.setState(calculate(this.state, buttonName));
 }
-  render () {
-    return (
+
+render() {
+  return (
     <div className="App">
       <header className="App-header">
         <div className="calc-body">
-          <Display value={ this.state.next || this.state.total || this.state.error || '0'} />
-          <ButtonPanel className = "button-panel" clickHandler = {this.handleClick} />
+          <Display value={this.state.next || this.state.total || this.state.error || '0'} />
+          <ButtonPanel className="button-panel" clickHandler={this.handleClick} />
         </div>
       </header>
     </div>
